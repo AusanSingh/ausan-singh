45ad92902b767fd3ca79c5551dcd0c76371f5204
Ausan Singh <ausan.singh@vfirst.com>
2021-06-17 12:22:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ausan-singh';
+  title = 'Ausan Singh - UI Developer';
+  year = new Date().getFullYear();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './sections/about/about.component';
+import { EducationComponent } from './sections/education/education.component';
+import { ExperienceComponent } from './sections/experience/experience.component';
+import { HeaderComponent } from './sections/header/header.component';
+import { PortfolioComponent } from './sections/portfolio/portfolio.component';
+import { SkillsComponent } from './sections/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
